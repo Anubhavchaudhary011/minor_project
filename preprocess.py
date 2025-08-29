82458dfd0723c2d5b53eb474cb91994bd159c7f9
Anubhavchaudhary011 <anubhavsrdav20@gmail.com>
2025-08-29 20:09:02 +0530

    final

--- preprocess.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def clean_text(t: str) -> str:
-    if not isinstance(t, str):
-        t = str(t)
-    t = t.lower()
-    t = re.sub(r'[^a-z\s]', ' ', t)
-    t = re.sub(r'\s+', ' ', t).strip()
-    return t
